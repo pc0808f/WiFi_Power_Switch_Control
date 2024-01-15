7a48181d0f713990e1d9abf49b36afb6ee2d05c5
Sam Yang <pc0808f@gmail.com>
2024-01-15 13:45:18 +0800

    完成部份MAIN 連接WIFI 預設系統在電供上電後3秒才送電 黃燈常亮 進入OTA中 綠燈0.5秒閃 開機上電 綠燈常亮 正常連到WIFI

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from machine import Pin, Timer
 import machine
 import neopixel
 import time
+from time import sleep
 import network
+import wifimgr
+from machine import WDT
+import ntptime
+import os
 
 # 定義IO口
 IO_SW1_PIN = 0   # 輕觸按鍵
@@ -65,6 +70,34 @@ def check_wifi(timer):
     if wlan.isconnected():
         wifi_connected = True
         timer.deinit()
+        
+
+def tw_ntp(host='clock.stdtime.gov.tw', must=False):
+  """
+  host: 台灣可用的 ntp server 如下可任選，未指定預設為 clock.stdtime.gov.tw
+    tock.stdtime.gov.tw
+    watch.stdtime.gov.tw
+    time.stdtime.gov.tw
+    clock.stdtime.gov.tw
+    tick.stdtime.gov.tw
+  must: 是否非對到不可
+  """ 
+  ntptime.NTP_DELTA = 3155673600 # UTC+8 的 magic number
+  ntptime.host = host
+  count = 1
+  if must:
+    count = 100
+  for _ in  range(count):
+    try:
+      ntptime.settime()
+    except:
+      sleep(1)
+      continue
+    else:
+      return True
+  return False
+
+
 
 
 # 設置 LED 閃爍定時器
@@ -73,22 +106,26 @@ led_color=3
 led_timer.init(period=500, mode=Timer.PERIODIC, callback=toggle_led)
 
 
-# Wi-Fi 設定
-ssid = "Sam&Betty"
-password = "0928666624"
+# Wi-Fi 設定 讀取wifi.dat
+profiles = wifimgr.read_profiles()
 
 # Wi-Fi 連接
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 if not wlan.isconnected():
-    wlan.connect(ssid, password)
+    wlan.connect(profiles["name"], profiles[profiles["name"]])
+    
+sleep(3)
+
+wdt=WDT(timeout=1000*60*5) 
+
 
 # 設置檢查 Wi-Fi 連接定時器
 wifi_check_timer = Timer(1)
 wifi_check_timer.init(period=1000, mode=Timer.PERIODIC, callback=check_wifi)
 
 
-# 主迴圈
+# 檢查WIFI的主迴圈
 while not wifi_connected:
     if IO_SW1.value() == 0:  # 按鍵被按下
         reset_timer += 1
@@ -112,29 +149,67 @@ led_timer.deinit()
 # 停止 Wi-Fi 連接檢查定時器
 wifi_check_timer.deinit()
 
+# 取得時間
+tw_ntp(must=True)
 
+# 取得網路和時間後，開始進入OTA判斷
+
+
+# 檔案名稱
+filename = 'otalist.dat'
+
+# 取得目錄下的所有檔案和資料夾
+file_list = os.listdir()
+
+print(file_list)
+# 檢查檔案是否存在
+if filename in file_list:
+    # 在這邊要做讀取OTA列表，然後進行OTA的執行
+    print("OTA檔案存在")
+    
+    # 黃色代表OTA執行中
+    np[0]=rainbow_colors[2]
+    np.write()
+
+    try:
+        with open(filename) as f:
+            lines = f.readlines()[0].strip()
+
+        lines = lines.replace(' ', '')
+
+        # 移除字串中的雙引號和空格，然後使用逗號分隔字串
+        file_list = [file.strip('"') for file in lines.split(',')]
+
+        OTA = senko.Senko(
+            user="pc0808f",  # Required
+            repo="WiFi_Power_Switch_Control",  # Required
+            branch="main",  # Optional: Defaults to "master"
+            working_dir="release",  # Optional: Defaults to "app"
+            files=file_list
+        )
+
+        # 這邊要做OTA的執行，如果置成功，就會進行重啟，如果失敗，就要重新執行
+        while True:
+            if OTA.update():
+                print("Updated to the latest version! Rebooting...")
+                os.remove(filename)
+                machine.reset()
+            else:
+               print("Updated error! Rebooting...")
+
+    except:
+      print("Updated error! Rebooting...")
+    os.remove(filename)
+else:
+    print("OTA檔案不存在")
+
+
+print("ESP OTA OK")
 
 while True:
-   
-    if IO_SW1.value() == 0: # 按鍵按下
-        # 按一次，Power_EN反轉
-        Power_EN = 1-Power_EN
-        IO_5V_EN.value(Power_EN)
-        IO_12V_EN.value(Power_EN)
-        # 等待按鍵釋放
-        while IO_SW1.value() == 0:    
-            # 持續一段時間，這裡是0.1秒
-            time.sleep(0.1)
-
-    else:    # 按鍵放開 
- 
-        color = color + 1
-        if color == rainbow_colors_count:
-            color = 0
-            
-        # 依次設置每個LED的顏色
-        #np[0] = rainbow_colors[color]
-        #np.write()
-
-        # 持續一段時間，這裡是0.5秒
-        time.sleep(0.5)
+    try:
+        print("執行Power_Switch_Main.py...")
+        execfile('Power_Switch_Main.py')
+    except:
+        print("執行失敗，改跑Power_Switch_Main.mpy")
+        __import__('Power_Switch_Main.mpy')   
